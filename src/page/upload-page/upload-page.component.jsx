25254ef5363fc4a84d3891734928cbad84c1e584
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-07-15 19:54:10 +0530

    Added Upload Page and Form Input component

--- src/page/upload-page/upload-page.component.jsx
+++ src/page/upload-page/upload-page.component.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CustomButton from '../../components/custom-buttom/custom-button.component';
+import FormInput from '../../components/form-input/form-input.component';
 
 import './upload-page.styles.scss';
 
-const UploadPage = () => (
-    <div className='upload-page-container'>
-        <h1> Upload </h1>
-    </div>
-);
+const username = 'xyz';
+
+const UploadPage = () => {
+
+    
+    const username = 'aniak100'
+    const name = 'Ania Kubow'
+    const avatar = 'https://i.imgur.com/QwZod6m.png'
+    const [caption, setCaption] = useState('');
+    const [url, setUrl] = useState('');
+    const today = new Date()
+    const timestamp = today.toISOString()
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        console.log('Handle Submit');
+    };
+
+    return (
+        <div className='upload-page-container'>
+            <div className="page-header">
+                <h1>Upload video</h1>
+                <p>This video will be published to @{ username }</p>
+                <form onSubmit={ handleSubmit }>
+                    <div className='form-container'>
+                        <div className="image-upload"></div>
+                        <div className="form-section">
+                            <FormInput
+                                name='caption'
+                                label='caption'
+                                handleChange={ (event) => setCaption(event.target.value) }
+                                value={ caption }
+                                type='input'
+                                required
+                            />
+                            <FormInput
+                                name='video'
+                                label='video url'
+                                handleChange={ (event) => setUrl(event.target.value) }
+                                value={ url }
+                                type='input'
+                                required
+                            />
+                        </div>
+                    </div>
+                    <CustomButton alternate type='submit'>Upload</CustomButton>
+                </form>
+            </div>
+        </div>
+    );
+};
 
 export default UploadPage;
