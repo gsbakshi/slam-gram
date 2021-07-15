5930ca088212cff0d5e07cb65b631effc821c1d2
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-07-15 23:56:53 +0530

    Added Post Call for Upload Method with Axios

--- src/page/upload-page/upload-page.component.jsx
+++ src/page/upload-page/upload-page.component.jsx
@@ -1,25 +1,63 @@
-import React, {useState} from 'react';
-import CustomButton from '../../components/custom-buttom/custom-button.component';
-import FormInput from '../../components/form-input/form-input.component';
+import React, { useState } from 'react';
+import axios from 'axios';
+import faker from 'faker';
 
 import './upload-page.styles.scss';
 
-const username = 'xyz';
+import CustomButton from '../../components/custom-buttom/custom-button.component';
+import FormInput from '../../components/form-input/form-input.component';
+
+const createHeadingStyle = 'color: #49CC90; font-weight: bold; font-size: 25px';
+const responseStyle = 'font-weight: bold';
 
 const UploadPage = () => {
-
     
     const username = 'aniak100'
     const name = 'Ania Kubow'
     const avatar = 'https://i.imgur.com/QwZod6m.png'
-    const [caption, setCaption] = useState('');
-    const [url, setUrl] = useState('');
     const today = new Date()
     const timestamp = today.toISOString()
+    
+    const [caption, setCaption] = useState('');
+    const [videoUrl, setVideoUrl] = useState('');
+    
+    let id = faker.datatype.uuid();
 
     const handleSubmit = async event => {
         event.preventDefault();
-        console.log('Handle Submit');
+        
+        const data = {
+            id: id,
+            name: name,
+            username: username,
+            avatar: avatar,
+            is_followed: false,
+            video: videoUrl,
+            caption: caption,
+            likes: 0,
+            comments: 0,
+            timestamp: timestamp,
+            button_visible: false
+        }
+
+        try {
+            //                                                                                          *
+            console.log('%cUpload Tik Tok Post', createHeadingStyle);
+            console.log('%cPATH /.netlify/functions/add using AXIOS POST', responseStyle);
+            console.log("%cWith the following BODY = ", responseStyle);
+            console.log("%c\t%s", responseStyle, JSON.stringify(data));
+            //                                                                                          *
+            
+            let response = await axios.post('/.netlify/functions/add', data);
+            
+            //                                                                                          *
+            console.log('%cResponse from Getting Rest Todos', responseStyle);
+            console.log(response);
+            //                                                                                          *
+
+        } catch (error) {
+            console.error(error);
+        }
     };
 
     return (
@@ -37,15 +75,13 @@ const UploadPage = () => {
                                 handleChange={ (event) => setCaption(event.target.value) }
                                 value={ caption }
                                 type='input'
-                                required
                             />
                             <FormInput
                                 name='video'
                                 label='video url'
-                                handleChange={ (event) => setUrl(event.target.value) }
-                                value={ url }
+                                handleChange={ (event) => setVideoUrl(event.target.value) }
+                                value={ videoUrl }
                                 type='input'
-                                required
                             />
                         </div>
                     </div>
